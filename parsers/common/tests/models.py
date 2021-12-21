4ba87d968ef727b080564675cc82c949a6959555
Georg Osang <geoo89@gmail.com>
2021-12-21 22:38:30 +0100

    Parser to turn rows into instances of a specified model

---
+++ parsers/common/tests/models.py
@@ -0,0 +1,20 @@
+from typing import List
+from parsers.common.rowparser import ParserModel
+
+
+class Condition(ParserModel):
+    value: str = ''
+    var: str = ''
+    type: str = ''
+    name: str = ''
+    # TODO: We could specify proper default values here, and write custom
+    # validators that replace '' with the actual default value.
+
+
+class FromWrong(ParserModel):
+    # 'Wrong' because this doesn't make too much sense:
+    # We'd be associating a single edge with multiple conditions.
+    # It's still useful for the testcases we have though.
+    row_id: str
+    conditions: List[Condition]
+
