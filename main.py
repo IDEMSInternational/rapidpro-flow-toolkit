7d64d732c26a2718bb0a19894b0564e45fa38398
Georg Osang <geoo89@gmail.com>
2022-11-25 12:39:49 +0100

    Main console tool

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
 import json
 import argparse
-import logging
-import csv
-import os
 
-from parsers.creation.standard_parser import Parser
+from parsers.creation.contentindexparser import ContentIndexParser
+from parsers.sheets.csv_sheet_reader import CSVSheetReader
 from rapidpro.models.containers import RapidProContainer
 
-def get_dict_from_csv(filename):
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        return [row for row in csv_reader]
 
 def main():
-    description = 'Generate RapidPro JSON from Spreadsheet.\n\n'\
-                  'Example input: tests/input/master_sheet.csv\n'\
-                  'Note: (Master) sheet parsing will be a separate component and'\
-                  ' is only included in here as a proof of concept.'
+    description = 'Generate RapidPro JSON from Spreadsheet(s).\n\n'\
+                  'Example usage: \n'\
+                  'create_flows tests/input/example1/content_index.csv out.json --format=csv --datamodels=tests.input.example1.nestedmodel'
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('input', help='Master spreadsheet listing all sheets to be processed (CSV).')
+    parser.add_argument('command', choices=["create_flows", "flow_to_sheet"], help='What to do.')
+    parser.add_argument('input', help='Content index sheet defining flows to be created.')
     parser.add_argument('output', help='RapidPro JSON file to write the output to.')
-    parser.add_argument('--logfile', help='File to log warnings and errors to.')
+    parser.add_argument('--format', required=True, choices=["csv", "xlsx", "googlesheet"], help='Format of the content index sheet.')
+    parser.add_argument('--datamodels', help='Module defining models for data sheets. E.g. if the definitions reside in ./myfolder/mysubfolder/mymodelsfile.py, then this argument should be myfolder.mysubfolder.mymodelsfile')
     args = parser.parse_args()
 
-    if args.logfile:
-        logging.basicConfig(filename=args.logfile, level=logging.WARNING, filemode='w')
+    if args.command != 'create_flows':
+        print(f"Command {args.command} currently unsupported.")
+        return
 
-    # This functionality shouldn't be in main.py, but be done by a dedicated master sheet parser.
-    rpc = RapidProContainer()
-    sheets = get_dict_from_csv(args.input)
-    for sheet in sheets:
-        if sheet['status'] != 'released' or sheet['flow_type'] != 'flow':
-            continue
-        rows = get_dict_from_csv(os.path.join(os.path.dirname(args.input), sheet['sheet_name'] + '.csv'))
-        parser = Parser(rows=rows, flow_name=sheet['flow_name'])
-        parser.parse()
-        rpc.add_flow(parser.get_flow())
-    rpc.update_global_uuids()
-    json.dump(rpc.render(), open(args.output, 'w'), indent=4)
+    if args.format == 'csv':
+        sheet_reader = CSVSheetReader(args.input)
+    else:
+        print(f"Format {args.format} currently unsupported.")
+        return
+
+    ci_parser = ContentIndexParser(sheet_reader, args.datamodels)
+    output = ci_parser.parse_all_flows()
+    json.dump(output.render(), open(args.output, 'w'), indent=4)
+    
 
 if __name__ == '__main__':
     main()
