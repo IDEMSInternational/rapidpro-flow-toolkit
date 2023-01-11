a3e7f02b873dba9e70a07338c4a321e14a8d3546
fagiothree <32491856+fagiothree@users.noreply.github.com>
2023-01-11 19:08:00 +0100

    Update parenttext_models.py

--- tests/input/parenttext/parenttext_models.py
+++ tests/input/parenttext/parenttext_models.py
@@ -12,13 +12,66 @@ class HandlerModel(DataRowModel):
 	split_variable: str = ''
 	module: List[str] = []
 
-
-
 class ModuleModel(DataRowModel):
 	age_baby: bool = True
 	age_child: bool = True
 	age_teen: bool = True
 	template_type: str = ''
 
+class TemplateTypeModel(DataRowModel):
+	blocks: List[str] = []
+
+
+# block models
+class IntroductionBlockModel(DataRowModel):
+	skill_introduction_msg_list: List[str] = []
+	skill_importance: str = ''
+
+
+class SurveyBlockModel(DataRowModel):
+	survey_ID: str = ''
+	survey_intro: str = ''
+
+
+class SurveyQuestionModel(DataRowModel):
+	survey_question: str = ''
+	max_val: int = 7
+	improvement: str = ''
+	threshold_positive: int = 4
+	feedback_better: List[str] = []
+	feedback_worse: List[str] = []
+
+class VideoBlockModel(DataRowModel):
+	video_message: str = ''
+	old_video_message: str = ''
+	video_file: str = ''
+	audio_message: str = ''
+	audio_file: str = ''
+	expiration_time_minutes: int = 2
+
+class CongratulationsBlockModel(DataRowModel):
+	congratulations_message: str = ''
+
+class HomeActivityBlockModel(DataRowModel):
+	home_activity: str = ''
+
+class ComicBlockModel(DataRowModel):
+	comic_introduction: str = ''
+	comic_attachment: str = ''
+	n_comic_attachments: int = 0
+	comic_text: List[str] = []
+
+"""
+class InteractionBlockModel(DataRowModel):
+
+
+class TipsBlockModel(DataRowModel):
+
+
+
+
+"""
+
+# old - delete?
 class ContentModel(DataRowModel):
 	message: str = ''
