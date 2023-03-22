071383b2e3bb499c7547cc93e930f7ad68437d92
fagiothree <32491856+fagiothree@users.noreply.github.com>
2023-03-22 15:17:45 +0100

    Update parenttext_models_delivery.py

--- tests/input/parenttext/parenttext_models_delivery.py
+++ tests/input/parenttext/parenttext_models_delivery.py
@@ -8,20 +8,12 @@ class GoalModel(DataRowModel):
 	modules: List[str] = []
 
 
-
-'''
 class HandlerModel(DataRowModel):
 	split_variable: str = ''
-	module: List[str] = []
+	flow_name: str = ''
+
 
 class ModuleModel(DataRowModel):
 	age_baby: bool = True
 	age_child: bool = True
 	age_teen: bool = True
-	template_type: str = ''
-
-class TemplateTypeModel(DataRowModel):
-	blocks: List[str] = []
-
-
-'''
