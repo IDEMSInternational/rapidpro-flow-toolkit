f8843bf57f229a3185a6dd9d34afe23614faea60
Ed Moss <ed.moss@eemengineering.com>
2023-06-07 15:28:30 +0100

    responding to Ian's comments

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='rapidpro_flow_tools',
     version='2.0.3',
-    license='MIT',
-    author='Ed Moss',
-    author_email='ed.moss@eemengineering.com',
+    license='LICENSE',
+    author='IDEMS International',
+    author_email='communications@idems.international',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/IDEMSInternational/rapidpro-flow-toolkit',
