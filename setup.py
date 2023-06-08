3ab73300e05d757590541bf85e5e4876b9dcb90c
Ed Moss <ed.moss@eemengineering.com>
2023-06-08 12:26:55 +0100

    adding package reference files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rapidpro_flow_tools',
+    version='2.0.4',
+    license='LICENSE',
+    author='IDEMS International',
+    author_email='communications@idems.international',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/IDEMSInternational/rapidpro-flow-toolkit',
+    keywords='rapidpro flow tools',
+)
