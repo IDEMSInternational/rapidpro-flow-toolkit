0544b2b15ca6c02294d3b7d46b041830ce7c8075
Ian Stride <ianstride@gmail.com>
2024-07-24 20:54:42 +0100

    Tidy tests for sheet parser

--- tests/test_sheetparser.py
+++ tests/test_sheetparser.py
@@ -1,55 +1,89 @@
-import unittest
-import tablib
+from unittest import TestCase
 
-from rpft.parsers.common.rowparser import ParserModel
+import tablib
 from rpft.parsers.common.sheetparser import SheetParser
+
 from tests.mocks import MockRowParser
 
 
-class MainModel(ParserModel):
-    field1: str = ""
-    field2: str = ""
-
-
-input1 = """field1,field2
-row1f1,row1f2
-row2f1,row2f2
-row3f1,row3f2
-"""
-
-
-class TestSheetParser(unittest.TestCase):
+class TestSheetParser(TestCase):
     def setUp(self):
-        self.rowparser = MockRowParser()
-        self.table1 = tablib.import_set(input1, format="csv")
+        self.parser = SheetParser(
+            MockRowParser(),
+            tablib.Dataset(
+                ("row1f1", "row1f2"),
+                ("row2f1", "row2f2"),
+                ("row3f1", "row3f2"),
+                headers=("field1", "field2"),
+            ),
+        )
 
     def test_context_and_bookmarks(self):
-        parser = SheetParser(self.rowparser, self.table1)
-        row1 = parser.parse_next_row()
-        self.assertEqual(row1, {"field1": "row1f1", "field2": "row1f2", "context": {}})
-        parser.create_bookmark("row2")
-        parser.add_to_context("key", "value")
-        row2 = parser.parse_next_row()
         self.assertEqual(
-            row2, {"field1": "row2f1", "field2": "row2f2", "context": {"key": "value"}}
+            self.parser.parse_next_row(),
+            {
+                "field1": "row1f1",
+                "field2": "row1f2",
+                "context": {},
+            },
         )
-        parser.remove_from_context("key")
-        row3 = parser.parse_next_row()
-        self.assertEqual(row3, {"field1": "row3f1", "field2": "row3f2", "context": {}})
-        parser.go_to_bookmark("row2")
-        row2b = parser.parse_next_row()
-        self.assertEqual(row2b, {"field1": "row2f1", "field2": "row2f2", "context": {}})
+
+        self.parser.create_bookmark("row2")
+        self.parser.add_to_context("key", "value")
+        self.assertEqual(
+            self.parser.parse_next_row(),
+            {
+                "field1": "row2f1",
+                "field2": "row2f2",
+                "context": {"key": "value"},
+            },
+        )
+
+        self.parser.remove_from_context("key")
+        self.assertEqual(
+            self.parser.parse_next_row(),
+            {
+                "field1": "row3f1",
+                "field2": "row3f2",
+                "context": {},
+            },
+        )
+
+        self.parser.go_to_bookmark("row2")
+        self.assertEqual(
+            self.parser.parse_next_row(),
+            {
+                "field1": "row2f1",
+                "field2": "row2f2",
+                "context": {},
+            },
+        )
 
     def test_parse_all(self):
-        parser = SheetParser(self.rowparser, self.table1)
-        rows = parser.parse_all()
+        rows = self.parser.parse_all()
+
         self.assertEqual(len(rows), 3)
         self.assertEqual(
-            rows[0], {"field1": "row1f1", "field2": "row1f2", "context": {}}
+            rows[0],
+            {
+                "field1": "row1f1",
+                "field2": "row1f2",
+                "context": {},
+            },
         )
         self.assertEqual(
-            rows[1], {"field1": "row2f1", "field2": "row2f2", "context": {}}
+            rows[1],
+            {
+                "field1": "row2f1",
+                "field2": "row2f2",
+                "context": {},
+            },
         )
         self.assertEqual(
-            rows[2], {"field1": "row3f1", "field2": "row3f2", "context": {}}
+            rows[2],
+            {
+                "field1": "row3f1",
+                "field2": "row3f2",
+                "context": {},
+            },
         )
