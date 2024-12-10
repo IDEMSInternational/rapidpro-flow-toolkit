d2ee1ba1f5a02c4a059cea04ee4bb717518f7101
Ian Stride <ianstride@gmail.com>
2024-12-10 19:56:26 +0000

    Document class attributes with attribute docstrings

--- src/rpft/parsers/creation/models.py
+++ src/rpft/parsers/creation/models.py
@@ -4,14 +4,25 @@ from rpft.parsers.common.rowparser import ParserModel
 
 
 class Condition(ParserModel):
-    # Value to check against. (Does not support checks with a list of values)
     value: str = ""
-    # Variable or expression that is being checked against value
+    """
+    Value to check against. (Does not support checks with a list of values).
+    """
+
     variable: str = ""
-    # Type of the conditional check, e.g. has_any_word (default)
+    """
+    Variable or expression that is being checked against value.
+    """
+
     type: str = ""
-    # Name -- only for display in RapidPro
+    """
+    Type of the conditional check, e.g. has_any_word (default).
+    """
+
     name: str = ""
+    """
+    Display name in RapidPro only.
+    """
 
 
 class ConditionWithMessage(ParserModel):
@@ -25,16 +36,24 @@ class ConditionsWithMessage(ParserModel):
 
 
 class Assignment(ParserModel):
-    # Assign a value to a variable
+    """
+    Assign a value to a variable
+    """
+
     variable: str
     value: str
 
 
 class Expiration(ParserModel):
-    # Time after which a flow expires
     time: str = ""
-    # Message to send when the flow expires
+    """
+    Time after which a flow expires.
+    """
+
     message: str = ""
+    """
+    Message to send when the flow expires.
+    """
 
 
 class Message(ParserModel):
@@ -76,77 +95,138 @@ class ChatbotDefinition:
 
 
 class SurveyConfig(ParserModel):
-    # Prefix to apply to all variable names in the survey
     variable_prefix: str = ""
-    # Message to send when a question flow expires
+    """
+    Prefix to apply to all variable names in the survey.
+    """
+
     expiration_message: str = ""
+    """
+    Message to send when a question flow expires.
+    """
 
 
 class MCQChoice(ParserModel):
-    # Text rendered to user
     text: str
-    # Text stored in survey variable
+    """
+    Text rendered to user.
+    """
+
     value: str
+    """
+    Text stored in survey variable.
+    """
 
 
 class PostProcessing(ParserModel):
-    # Assignments to perform via save_value rows
     assignments: List[Assignment] = []
-    # flow to invoke for postprocessing
+    """
+    Assignments to perform via save_value rows.
+    """
+
     flow: str = ""
+    """
+    Flow to invoke for postprocessing.
+    """
 
 
 class SkipOption(ParserModel):
-    # The text with instructions that the user sees
     text: str = ""
-    # The Choice (quick reply/text) the user has to enter to skip this question
+    """
+    The text with instructions that the user sees.
+    """
+
     choice: str = ""
-    # The value that is stored in the question variable if the question is skipped
+    """
+    The choice (quick reply/text) the user has to enter to skip this question.
+    """
+
     value: str = ""
+    """
+    The value that is stored in the question variable if the question is skipped.
+    """
 
 
 class SurveyQuestionModel(ParserModel):
-    # type of the question
-    type: str
-    # question text
-    messages: List[Message]
-    # Variable to store the user input in
-    # If blank, generated from the question ID as sq_{survey_id}_{question_id}
-    # The survey_id/question_id is the survey's name/question's ID
-    # in all lowercase with non-alphanumeric characters removed
-    variable: str = ""
-    # Variable indicating whether question has been completed
-    # If blank, generated from the question ID as {variable}_complete
-    completion_variable: str = ""
-    # MCQ specific fields
-    choices: List[MCQChoice] = []
+    """
+    Representation of a survey question.
+    """
 
-    # Message to send when question flow expires
-    # If blank, message from survey configuration is used
+    type: str
+    """
+    Type of the question.
+    """
+
+    messages: List[Message]
+    """
+    Question text.
+    """
+
+    variable: str = ""
+    """
+    Variable to store the user input in. If blank, generated from the question ID as
+    sq_{survey_id}_{question_id}. The survey_id/question_id is the survey's
+    name/question's ID in all lowercase with non-alphanumeric characters removed.
+    """
+
+    completion_variable: str = ""
+    """
+    Variable indicating whether question has been completed. If blank, generated from
+    the question ID as {variable}_complete.
+    """
+
+    choices: List[MCQChoice] = []
+    """
+    MCQ specific fields.
+    """
+
     expiration: Expiration = Expiration()
-    # Conditions required to present the question, otherwise skipped.
+    """
+    Message to send when question flow expires. If blank, message from survey
+    configuration is used.
+    """
+
     relevant: List[Condition] = []
-    ## Make question skippable
-    # Adds an additional choice allowing use to skip the question.
+    """
+    Conditions required to present the question, otherwise skipped.
+    """
+
     skipoption: SkipOption = SkipOption()
-    ## Conditional Answer confirmation
-    # Condition for sending a message asking the user to confirm their answer
-    # Comes with a Yes/No choice for the user.
-    # No repeats the question, Yes stores the answer and proceeds.
+    """
+    Make question skippable. Adds an additional choice allowing use to skip the
+    question.
+    """
+
     confirmation: ConditionsWithMessage = ConditionsWithMessage()
-    ## Conditional premature end of survey (later: forward skip?)
-    # Condition that ends the survey (with message to user)
+    """
+    Conditional Answer confirmation. Condition for sending a message asking the user to
+    confirm their answer. Comes with a Yes/No choice for the user. 'No' repeats the
+    question; 'Yes' stores the answer and proceeds.
+    """
+
     stop: ConditionsWithMessage = ConditionsWithMessage()
-    ## Validation / conditional repetition of question
-    # If condition holds, message is printed and question is repeated.
+    """
+    Conditional premature end of survey (later: forward skip?). Condition that ends the
+    survey (with message to user).
+    """
+
     validation: ConditionsWithMessage = ConditionsWithMessage()
-    ## Variable postprocessing
-    # Postprocessing to do after a user's answer is successfully stored
-    # This could be an assignment (of the same or another variable),
-    # Or a flow that is triggered.
+    """
+    Validation / conditional repetition of question. If condition holds, message is
+    printed and question is repeated.
+    """
+
     postprocessing: PostProcessing = PostProcessing()
-    # tags allowing to filter questions to appear in a survey
+    """
+    Variable postprocessing. Postprocessing to do after a user's answer is successfully
+    stored. This could be an assignment (of the same or another variable), or a flow
+    that is triggered.
+    """
+
     tags: List[str] = []
+    """
+    Tags allowing to filter questions to appear in a survey.
+    """
 
     def header_name_to_field_name_with_context(header, row):
         if header == "question":
