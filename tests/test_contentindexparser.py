cee08467efa12a7c71d9f74bc33f5a9c0bcf51b5
Ian Stride <ianstride@gmail.com>
2025-03-25 11:32:37 +0000

    Fix issue where operations appear to be applied more than once (#167)
    Co-authored-by: Georg Os. <geoo89@gmail.com>

--- tests/test_contentindexparser.py
+++ tests/test_contentindexparser.py
@@ -3,8 +3,14 @@ from unittest import TestCase
 
 from rpft.parsers.creation.contentindexparser import ContentIndexParser
 from rpft.parsers.creation.tagmatcher import TagMatcher
-from rpft.parsers.sheets import CompositeSheetReader, CSVSheetReader, XLSXSheetReader
+from rpft.parsers.sheets import (
+    CompositeSheetReader,
+    CSVSheetReader,
+    DatasetSheetReader,
+    XLSXSheetReader,
+)
 from rpft.rapidpro.models.triggers import RapidProTriggerError
+from tablib import Dataset
 from tests import TESTS_ROOT
 from tests.mocks import MockSheetReader
 from tests.utils import Context, csv_join, traverse_flow
@@ -745,6 +751,72 @@ class TestParsing(TestTemplate):
             {flow["name"] for flow in rapidpro_export["flows"]},
             flow_names,
         )
+
+
+class TestOverrideBehaviour(TestCase):
+
+    def test_data_defined_earlier_is_overridden_by_later_definitions(self):
+        base = DatasetSheetReader(
+            [
+                Dataset(
+                    ("A", "", ""),
+                    ("B", "", ""),
+                    headers=("ID", "value1", "value2"),
+                    title="data",
+                ),
+                Dataset(
+                    ("data_sheet", "data", "", "SimpleRowModel"),
+                    headers=("type", "sheet_name", "new_name", "data_model"),
+                    title="content_index",
+                ),
+            ],
+            "base",
+        )
+        filter_ = DatasetSheetReader(
+            [
+                Dataset(
+                    (
+                        "data_sheet",
+                        "data",
+                        "data",
+                        "SimpleRowModel",
+                        'filter | expression ; ID != "B"',
+                    ),
+                    headers=(
+                        "type",
+                        "sheet_name",
+                        "new_name",
+                        "data_model",
+                        "operation",
+                    ),
+                    title="content_index",
+                )
+            ],
+            "filter",
+        )
+        deployment = DatasetSheetReader(
+            [
+                Dataset(
+                    ("B", "", ""),
+                    ("C", "", ""),
+                    headers=("ID", "value1", "value2"),
+                    title="data",
+                ),
+                Dataset(
+                    ("data_sheet", "data", "data", "SimpleRowModel"),
+                    headers=("type", "sheet_name", "new_name", "data_model"),
+                    title="content_index",
+                ),
+            ],
+            "deployment",
+        )
+        definition = ContentIndexParser(
+            CompositeSheetReader([base, filter_, deployment]),
+            "tests.datarowmodels.simplemodel",
+        ).definition
+        keys = list(definition.get_data_sheet_rows("data").keys())
+
+        self.assertEqual(keys, ["B", "C"], "Deployment dataset should prevail")
 
 
 class TestConcatOperation(TestCase):
